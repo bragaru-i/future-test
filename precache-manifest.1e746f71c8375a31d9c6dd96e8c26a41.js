514c1f8b63e746ee33fbe882e477c2dde65efa44
fargusMD <bragaruion@gmail.com>
2020-07-30 01:13:23 +0400

    Updates

---
+++ precache-manifest.1e746f71c8375a31d9c6dd96e8c26a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3af2a0b450e796f0259a7529d83a07",
+    "url": "/future-test/index.html"
+  },
+  {
+    "revision": "9a7e1a3cb982e7292912",
+    "url": "/future-test/static/css/main.893e6de2.chunk.css"
+  },
+  {
+    "revision": "77cf062188638a349d27",
+    "url": "/future-test/static/js/2.77eabcb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/future-test/static/js/2.77eabcb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7e1a3cb982e7292912",
+    "url": "/future-test/static/js/main.69d03dba.chunk.js"
+  },
+  {
+    "revision": "f75b0feaa8c6b8db234e",
+    "url": "/future-test/static/js/runtime-main.65271f8e.js"
+  }
+]);
