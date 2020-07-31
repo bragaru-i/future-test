cb5982735ab8cf0963bbc879fab04dc8b9987840
fargusMD <bragaruion@gmail.com>
2020-07-31 12:16:02 +0400

    .

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -36,8 +36,11 @@ function App() {
         ? 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
         : 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
     setData((prevState) => ({ ...prevState.data, isLoading: true }));
-    axios
-      .get(`https://cors-anywhere.herokuapp.com/${dbUrl}`, )
+   axios({
+    method: 'get',
+    url:`https://cors-anywhere.herokuapp.com/${dbUrl}`,
+    headers: {'Origin': 'https://example.com'}
+})
       .then((res) => {
         setData((prevState) => ({
           ...prevState,
